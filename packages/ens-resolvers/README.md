499fa202b00920a6322f22d20679e28e1a6ee004
Michael Barlock <barlockm@gmail.com>
2018-11-29 09:55:11 -0500

    Initialize lerna, eslint, and prettier

---
+++ packages/ens-resolvers/README.md
@@ -0,0 +1,11 @@
+# `ens-resolvers`
+
+> TODO: description
+
+## Usage
+
+```
+const ensResolvers = require('ens-resolvers');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/ens-resolvers/src/ens-resolvers.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = ensResolvers;
+
+/**
+ * Lerna bootstrapped code that will be deleted
+ */
+function ensResolvers() {
+  // TODO
+}
