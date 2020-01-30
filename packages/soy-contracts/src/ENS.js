d4051b1e904fc0da6db95a8cf9596d2fdbd52089
ensbot <58172776+ensbot@users.noreply.github.com>
2020-01-30 12:42:34 +0000

    My commit message -4712-01-01

--- packages/soy-contracts/src/ENS.js
+++ packages/soy-contracts/src/ENS.js
@@ -212,13 +212,13 @@ const ENS = {
   ],
   networks: {
     '1': {
-      address: '0x314159265dd8dbb310642f98f50c066173c1259b'
+      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
     },
     '3': {
-      address: '0x112234455c3a32fd11230c42e7bccd4a84e02010'
+      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
     },
     '4': {
-      address: '0xe7410170f87102df0055eb195163a03b7f2bff4a'
+      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
     }
   }
 };
